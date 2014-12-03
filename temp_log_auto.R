219c6cbc7de34d8ecbb192cc35b0629e14144a5a
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2014-12-04 00:14:26 +0100

    Automagic detect of more then one temperatur sensor is in use.
    Check if more then one sensor provide data to the log file and run
    dual plot graph. We currently do not support more then two temperatur
    sensors.

--- temp_log_auto.R
+++ temp_log_auto.R
@@ -21,8 +21,8 @@ colnames(log)<-c("datestamp", "temp1")
 # Find number of temperatur sensors.
 sensorer = ncol(log)/2
 
-# Create new column in log called "datetime" using datestamp read as POSIX
-# date-time factor.
+# Workaround: Create new column in log called "datetime" using datestamp read as
+# POSIX date-time factor.
 log$datetime<-as.POSIXct(log$datestamp, format = "%Y-%m-%d %H:%M:%S")
 
 # Setup and define plot device.
@@ -42,12 +42,6 @@ plot(
   yaxp = c(18,36,9)
 )
 
-points(
-  temp1~datetime,
-  data = log,
-  type = "l",
-  col = "red"
-)
 
 if (sensorer > 1) {
   points(
@@ -55,6 +49,33 @@ if (sensorer > 1) {
     data = log,
     type = "l",
     col = "darkgreen"
+  )
+
+  legend(
+    "bottomright",
+    c("ambient", "gjæringskar"),
+    pch = 22,
+    col = c("red", "darkgreen"),
+    pt.bg = c("red", "darkgreen"),
+    bty = "n",
+    cex = 1.5
+  )
+} else {
+  points(
+    temp1~datetime,
+    data = log,
+    type = "l",
+    col = "red"
+  )
+
+  legend(
+    "bottomright",
+    c("gjæringskar"),
+    pch = 22,
+    col = "red",
+    pt.bg = "red",
+    bty = "n",
+    cex = 1.5
   )
 }
 
@@ -65,16 +86,6 @@ axis.POSIXct(
   las = 2,
   format = "%Y/%m/%d %H:%M:%S",
   at = log$datetime[seq(1, length(log$datetime), 10000)]
-)
-
-legend(
-  "bottomright",
-  c("ambient", "gjæringskar"),
-  pch = 22,
-  col = c("red", "darkgreen"),
-  pt.bg = c("red", "darkgreen"),
-  bty = "n",
-  cex = 1.5
 )
 
 legend(
