f25b47d9cb9dd9ae0c99c713274a63e6387a3263
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2016-02-03 15:51:03 +0100

    Explicit define number of sensors. More then two does not really work.
    - fallback message if non was passed or found.

--- plotFallback.r
+++ plotFallback.r
@@ -13,7 +13,26 @@ plotFallback <- function(log = "und", sensorer = 0) {
     yaxp = c(min_temp, max_temp, 9)
   )
 
-  if (sensorer > 1) {
+  if (sensorer == 1) {
+    points(
+      temp1~datestamp,
+      data = log,
+      type = "l",
+      col = "red"
+    )
+
+    legend(
+      "bottomright",
+      c("gjæringskar"),
+      pch = 22,
+      col = "red",
+      pt.bg = "red",
+      bty = "n",
+      cex = 1.5
+    )
+  }
+
+  if (sensorer == 2) {
     points(
       temp2~datestamp,
       data = log,
@@ -38,23 +57,9 @@ plotFallback <- function(log = "und", sensorer = 0) {
       cex = 1.5
     )
   }
-  else {
-    points(
-      temp1~datestamp,
-      data = log,
-      type = "l",
-      col = "red"
-    )
 
-    legend(
-      "bottomright",
-      c("gjæringskar"),
-      pch = 22,
-      col = "red",
-      pt.bg = "red",
-      bty = "n",
-      cex = 1.5
-    )
+  if (sensorer == 0) {
+    writeLines("No sensors found. Giving up.")
   }
 
   axis.POSIXct(
