eb3e6abc3414ee568092f87b475a0fbf56c482dd
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2016-03-15 11:45:44 +0100

    Moved CSS into external style file.

--- www/index.php
+++ www/index.php
@@ -17,37 +17,21 @@ $fermentor1_trend = trend($fermentor1, $ambient, $data);
 $status = '<p class="temp">' . $sample_time . $ambient_status . $fermentor1_status . '</p>';
 
 ?>
-
 <html>
-<head>
-  <title>Gjæring</title>
-
-  <style>
-    body { color: #333; background-color: #fff; margin: 0 auto; }
-    .header { width: 100%; display: inline-block; z-index: 3; }
-    .temp { font-style: italic; margin: 0; text-align: right; }
-    .ambient { color: red; padding-left: 1em; }
-    .fermentor { color: green; padding-left: 1em; }
-    .title { width: 25%; float: left; text-transform: uppercase; margin: 0; z-index: 3; font-size: 1.1em }
-    .title {}
-    .temp { width: 75%; float: right; }
-    img { height: 95%; width: auto; z-index: 1; }
-    .content { }
-  </style>
-
-</head>
+<LINK REL=StyleSheet HREF="css/style.css" TYPE="text/css" MEDIA=screen>
 <meta charset="UTF-8" http-equiv="refresh" content="180">
 
-<body>
+<head>
+  <title>Gjæring</title>
+  <link>
+</head>
 
+<body>
   <div class="header">
     <h1 class="title">Brewpi temperature log</h1>
   </div>
-
   <div class="content">
     <img alt="temperatur log" src="temperatur.png">
   </div>
-
 </body>
 </html>
-
