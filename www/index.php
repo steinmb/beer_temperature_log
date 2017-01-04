1e8ec09d461e21e2324fb5f3a7b1b3f1eb9ac536
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2017-01-04 23:12:50 +0100

    Update index.php with log file new location, outside document root.

--- www/index.php
+++ www/index.php
@@ -4,20 +4,22 @@
  */
 
 define('BREW_ROOT', getcwd());
-$fileName = BREW_ROOT . '/' . 'temp.log';
+$fileName = BREW_ROOT . '/../../brewlogs/temperature.log';
 require_once BREW_ROOT . '/includes/dataSource.php';
 require_once BREW_ROOT . '/includes/Sensor.php';
 require_once BREW_ROOT . '/includes/DataEntity.php';
 require_once BREW_ROOT . '/includes/Block.php';
 require_once BREW_ROOT . '/includes/OldSensor.php';
 
-$source = file($fileName);
-$data = new DataSource($source);
-$sensors = new Sensor($data);
-$entities = $sensors->getEntities();
+if (file_exists($fileName)) {
+  $source = file($fileName);
+  $data = new DataSource($source);
+  $sensors = new Sensor($data);
+  $entities = $sensors->getEntities();
 
-foreach ($entities as $entity) {
-  $blocks[] = new Block($entity);
+  foreach ($entities as $entity) {
+    $blocks[] = new Block($entity);
+  }
+
+  include 'page.php';
 }
-
-include 'page.php';
