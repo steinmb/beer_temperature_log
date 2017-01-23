88aa3eb893f2a5ef5337b46ccb01be12932b9eef
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2017-01-23 21:14:29 +0100

    Rip out calculation from the data entity
    Put it into a separate class until we know what to do about it. Does not belong in the data
    entity or the data logger.

---
+++ www/includes/Calculate.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: steinmb
+ * Date: 23/01/2017
+ * Time: 21:04
+ */
+class Calculate {
+  private $data;
+  private $trend;
+
+  /**
+   * Get the last sample from temperature log.
+   *
+   * @return string of last temperature sample.
+   */
+  public function getLastReading()
+  {
+    $lastReading = $this->data[count($this->data) - 1];
+
+    return $lastReading;
+  }
+
+  /**
+   * Calculate trend of the last temperature readings.
+   *
+   * @param $time integer minutes to calculate latest trend from.
+   */
+  public function calculateTrend($time = 15)
+  {
+    $x = '';
+    $y = array();
+    $x2 = array();
+    $xy = array();
+    $last = $this->getLastReading();
+
+    foreach (array_reverse($this->data) as $key => $row) {
+      $y[] = 1000 * $row['Sensor'];
+      $x = $key + 1;
+      $x2[] = bcpow($x, $x);
+
+      if (strtotime($row['Date']) <= strtotime($last['Date']) - ($time * 60)) {
+        break;
+      }
+    }
+
+    $y = array_reverse($y);
+    $samples = $x;
+    $x = range(1, $x);
+
+    foreach ($x as $key => $item) {
+      $xy[] = $item * $y[$key];
+    }
+
+    $xSummary = array_sum($x);
+    $ySummary = array_sum($y);
+    $xySummary = array_sum($xy);
+
+    $x2Summary = 0;
+    foreach ($x2 as $item) {
+      $x2Summary = bcadd($x2Summary, $item, 10);
+    }
+
+    $vector1 = bcsub(bcmul($samples, $xySummary), bcmul($xSummary, $ySummary));
+    $vector2 = bcsub(bcmul($samples, $x2Summary), (bcsqrt($xSummary, 30)));
+    $this->trend = bcdiv($vector1, $vector2, 12);
+  }
+
+
+  /**
+   * Get entity trend data and round it down to 4 decimals.
+   *
+   * @return float.
+   */
+  public function getTrend()
+  {
+    return $this->trend;
+  }
+
+  /**
+   * Analyze trend index and calculate a human friendly label for it.
+   *
+   * @return string index label.
+   */
+  public function analyzeTrend()
+  {
+    $direction = 'increasing';
+
+    if ($this->trend < 0 ) {
+      $direction = 'decreasing';
+    }
+
+    $ranges = [
+      'stable' => 0.1,
+      'slowly' => 0.21,
+      'steady' => 0.3,
+      'medium' => 0.9,
+      'fast' => 2,
+    ];
+
+    $speed = '';
+    foreach ($ranges as $key => $range) {
+      if (ltrim($this->trend, '-') > $range) {
+        $speed =  $key;
+      }
+    }
+
+    return $direction . ' ' . $speed;
+  }
+}
