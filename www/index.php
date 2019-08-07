2f551f43f9f63c5137b1c614d46dc648b06a281c
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2019-08-07 12:29:40 +0200

    No reason to look for run time settings for the web interface

--- www/index.php
+++ www/index.php
@@ -9,34 +9,24 @@ declare(strict_types=1);
 
 define('BREW_ROOT', getcwd());
 define('SENSOR_DIRECTORY', '/sys/bus/w1/devices');
+define('LOG_DIRECTORY', BREW_ROOT . '/../../brewlogs/');
+define('LOG_FILENAME', 'temperature.log');
 
-$fileName = BREW_ROOT . '/../../brewlogs/temperature.log';
 require_once BREW_ROOT . '/includes/dataSource.php';
 require_once BREW_ROOT . '/includes/Sensor.php';
 require_once BREW_ROOT . '/includes/DataEntity.php';
 require_once BREW_ROOT . '/includes/Block.php';
 require_once BREW_ROOT . '/includes/OldSensor.php';
 require_once BREW_ROOT . '/includes/Logger.php';
+
 $entities = false;
+$sensors = new Sensor(SENSOR_DIRECTORY);
 
-/**
- * Check for runtime parameters and scan for attached sensors.
- */
-if ($argc > 1) {
-
-    if ($argv[1] === '--test') {
-        echo 'Running in test mode.' . PHP_EOL;
-        $sensors = new Sensor('./test');
-        if ($sensors) {
-            $entities = $sensors->createEntities();
-        }
-    } else {
-        echo 'Invalid argument. Valid arguments: --test' . PHP_EOL;
-        exit;
-    }
-} else {
-    $w1gpio = new OldSensor(SENSOR_DIRECTORY);
+if ($sensors) {
+    $entities = $sensors->createEntities();
 }
+
+$w1gpio = new OldSensor(SENSOR_DIRECTORY);
 
 if ($entities) {
     foreach ($entities as $entity) {
