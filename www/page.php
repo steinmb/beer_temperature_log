c89e7046541087601468c05bbcb5357018193c25
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2019-08-26 16:06:37 +0200

    Reformatting

--- www/page.php
+++ www/page.php
@@ -1,31 +1,30 @@
 <html>
 
-    <LINK REL=StyleSheet HREF="css/style.css" TYPE="text/css" MEDIA=screen>
-    <meta charset="UTF-8" http-equiv="refresh" content="180">
+<LINK REL=StyleSheet HREF="css/style.css" TYPE="text/css" MEDIA=screen>
+<meta charset="UTF-8" http-equiv="refresh" content="180">
 
-    <head>
-        <title>Ferment</title>
-        <link>
-    </head>
+<head>
+    <title>Ferment</title>
+    <link>
+</head>
 
-    <page>
-        <h1 class="title">Temperature log</h1>
+<page>
+    <h1 class="title">Temperature log</h1>
 
-        <body>
-            <img alt="temperatur log" src="temperatur.png">
+    <body>
+    <img alt="temperatur log" src="temperatur.png">
 
-            <div class="content">
+    <div class="content">
 
-              <?php
-              foreach ($blocks as $block)
-              {
-                print $block;
-              }
-              ?>
-            </div>
+        <?php
+        foreach ($blocks as $block) {
+            print $block;
+        }
+        ?>
+    </div>
 
-        </body>
+    </body>
 
-    </page>
+</page>
 
 </html>
