7e55d829c0a6f9ad91a6e32ea3cf5d0f2b311c35
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-02-01 01:09:26 +0100

    Demo check if sensors are found

--- www/demo.php
+++ www/demo.php
@@ -9,6 +9,10 @@ use steinmb\onewire\Temperature;
 $oneWire = new Sensor(__DIR__ . '/test');
 $sensors = $oneWire->getSensors();
 
+if (!$sensors) {
+    exit('No sensors found');
+}
+
 foreach ($sensors as $sensor) {
     $temp = new Temperature($sensor, __DIR__ . '/test');
     print "{$temp->id()} {$temp->temperature()} \n";
