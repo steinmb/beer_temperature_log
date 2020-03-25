0a96cefba06fb3c1f1823e30d480fffa18ed9778
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-03-25 13:34:21 +0100

    Refactor Logger
    Move Logger out of onewire. It do not really belong there.

--- www/demo.php
+++ www/demo.php
@@ -3,14 +3,24 @@ declare(strict_types=1);
 
 include_once __DIR__ . '/vendor/autoload.php';
 
+use steinmb\Logger\FileLogger;
+use steinmb\Logger\FileStorage;
 use steinmb\onewire\Sensor;
 use steinmb\onewire\SystemClock;
 use steinmb\onewire\OneWire;
 use steinmb\onewire\Temperature;
 
+define('BREW_ROOT', getcwd());
+
 $oneWire = new OneWire(__DIR__ . '/test');
 $sensor = new Sensor($oneWire, new SystemClock());
 $probes = $oneWire->getSensors();
+
+define('LOG_DIRECTORY', BREW_ROOT . '/../../brewlogs/');
+define('LOG_FILENAME', 'temperature.log');
+$log = new FileLogger(new FileStorage(
+  LOG_DIRECTORY,
+  LOG_FILENAME));
 
 foreach ($probes as $probe) {
     $temperature = new Temperature($sensor->createEntity($probe));
