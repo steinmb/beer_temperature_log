036f8591de6aa9bec86d690aca663cc81c17f46e
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-03-28 23:21:10 +0100

    Fix wrong namespace

--- www/src/steinmb/Formatters/Block.php
+++ www/src/steinmb/Formatters/Block.php
@@ -1,10 +1,9 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 namespace steinmb\Formatters;
 
 use steinmb\Logger\LoggerInterface;
 use steinmb\onewire\Temperature;
-use steinmb\steinmb\Formatters\HTMLFormatter;
 use steinmb\Utils\Calculate;
 
 /**
