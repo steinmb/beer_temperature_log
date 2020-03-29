d816f61ceee8a3ed3c18c7e46d9af6d95349cb5e
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-03-29 15:26:29 +0200

    Remove illegal HTML element
    -- Removal and reformatting

--- www/page.php
+++ www/page.php
@@ -1,25 +1,19 @@
-<html>
-
+<html lang="en">
 <LINK REL=StyleSheet HREF="css/style.css" TYPE="text/css" MEDIA=screen>
 <meta charset="UTF-8" http-equiv="refresh" content="180">
-
 <head>
     <title>Ferment</title>
     <link>
 </head>
 
-<page>
-    <h1 class="title">Temperature log</h1>
-
-    <body>
+<h1 class="title">Temperature log</h1>
+<body>
     <?php
     if (isset($graph)) {
         print '<img alt="temperatur log" class="graph" src="' . $graph . '">';
     }
     ?>
-
     <div class="content">
-
         <?php
 
         if (isset($blocks)) {
@@ -29,9 +23,5 @@
         }
         ?>
     </div>
-
-    </body>
-
-</page>
-
+</body>
 </html>
