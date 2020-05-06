6a651d44ef3fed6ae6ded8a4d2a01e2755033792
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-05-06 13:44:19 +0200

    Test default settings fallback

--- www/tests/EnvironmentTest.php
+++ www/tests/EnvironmentTest.php
@@ -5,6 +5,12 @@ use PHPUnit\Framework\TestCase;
 
 final class EnvironmentTest extends TestCase
 {
+
+    public function testDefault(): void
+    {
+        self::assertNotEquals('', Environment::getSetting('BREW_ROOT'));
+    }
+
     public function testSetting(): void
     {
         Environment::setSetting('DEMO_MODE', TRUE);
