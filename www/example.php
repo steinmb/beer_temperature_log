7206bb8b32c901036f62abf4c0adadee339dcd69
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-07-06 12:01:22 +0200

    Add simple example file
    Can be used in documentations etc.

---
+++ www/example.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+use steinmb\EntityFactory;
+use steinmb\Onewire\OneWire;
+use steinmb\Onewire\Sensor;
+use steinmb\RuntimeEnvironment;
+use steinmb\SystemClock;
+
+include_once __DIR__ . '/vendor/autoload.php';
+
+RuntimeEnvironment::setSetting('BREW_ROOT', __DIR__);
+
+$sensor = new Sensor(new OneWire(), new SystemClock(), new EntityFactory());
+$probes = (!$sensor->getTemperatureSensors()) ? exit('No probes found.'): $sensor->getTemperatureSensors();
