1b5037ac6c3b850fda7cfb5fcc96c66c6fea3cfd
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-09-22 21:48:29 +0200

    Stop reading files in test
    Time to make these real tests.

--- www/tests/TemperatureTest.php
+++ www/tests/TemperatureTest.php
@@ -1,12 +1,9 @@
 <?php declare(strict_types=1);
 
-use steinmb\EntityFactory;
-use steinmb\Onewire\DataEntity;
-use steinmb\Onewire\OneWire;
-use steinmb\Onewire\Sensor;
 use PHPUnit\Framework\TestCase;
+use steinmb\Onewire\DataEntity;
 use steinmb\Onewire\Temperature;
-use steinmb\SystemClock;
+use steinmb\SystemClockFixed;
 
 /**
  * Class TemperatureTest
@@ -15,38 +12,17 @@ use steinmb\SystemClock;
  */
 final class TemperatureTest extends TestCase
 {
-    private $sensor;
-
-    /**
-     * @var array
-     */
-    private $enties;
-
     private $temperature;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->sensor = new Sensor(
-          new OneWire(
-            __DIR__ . '/test_data',
-            __DIR__ . '/test_data/w1_master_slaves'
-          ),
-          new SystemClock(),
-          new EntityFactory()
-        );
-
-        foreach ($this->sensor->getTemperatureSensors() as $temperatureSensor) {
-            $this->enties[] = $this->sensor->createEntity($temperatureSensor);
-        }
-
         $this->temperature = new Temperature(new DataEntity(
             '28-1234567',
             'temperature',
             '20.123',
-            new \steinmb\SystemClockFixed(
-                new dateTimeImmutable('16.07.2018 13.01.00'))
-        ));
+            new SystemClockFixed(new dateTimeImmutable('16.07.2018 13.01.00')))
+        );
     }
 
     public function testCelsius(): void
@@ -61,16 +37,6 @@ final class TemperatureTest extends TestCase
             $this->temperature->temperature($unknownScale),
             'Failed detecting a uknow teperature scale.'
         );
-    }
-
-    public function testTemperature(): void
-    {
-        foreach ($this->enties as $enty) {
-            $temperature = new Temperature($enty);
-            $celsius = $temperature->temperature();
-            $measurement = $enty->measurement();
-            self::assertIsFloat($celsius);
-        }
     }
 
     public function testRawData(): void
