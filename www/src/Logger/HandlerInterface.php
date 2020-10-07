dc8cda8abb97b1492a1ab9f12790bb57820f637d
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-10-07 23:15:35 +0200

    Handler interface removed close method
    Not all implementations of interface will
    need ::close()

--- www/src/Logger/HandlerInterface.php
+++ www/src/Logger/HandlerInterface.php
@@ -3,12 +3,9 @@ declare(strict_types=1);
 
 namespace steinmb\Logger;
 
-use steinmb\RuntimeEnvironment;
-
 interface HandlerInterface
 {
     public function __construct();
     public function read();
     public function write(string $message);
-    public function close();
 }
