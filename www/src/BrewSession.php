5fc992a81b70b97b207fc755c7ed9a55348feedd
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2021-01-09 01:25:37 +0100

    Use brew session value class

---
+++ www/src/BrewSession.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace steinmb;
+
+class BrewSession
+{
+    public $sessionId;
+    public $probe;
+    public $ambient;
+    public $low_limit;
+    public $high_limit;
+
+    public function __construct(
+        string $sessionId = '',
+        string $probe = '',
+        string $ambient = '',
+        float $low_limit = 0,
+        float $high_limit = 0
+    )
+    {
+        $this->sessionId = $sessionId;
+        $this->probe = $probe;
+        $this->ambient = $ambient;
+        $this->low_limit = $low_limit;
+        $this->high_limit = $high_limit;
+    }
+}
