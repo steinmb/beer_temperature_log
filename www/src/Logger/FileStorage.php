19a9557449d8b67accc1c1b7c355aa03ba201c3c
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2021-02-01 20:15:42 +0100

    Last log entry for filehandlers broken
    If no last message have been written then
    last log entry is empty and we should load
    historic data from logfile.

--- www/src/Logger/FileStorage.php
+++ www/src/Logger/FileStorage.php
@@ -106,7 +106,74 @@ final class FileStorage implements HandlerInterface
 
     public function lastEntry(): string
     {
+        if (!$this->message) {
+            return $this->tailFile();
+        }
+
         return $this->message;
+    }
+
+    private function tailFile(int $lines = 1, bool $adaptive = true)
+    {
+        $f = @fopen($this->stream, "rb");
+        if ($f === false) {
+            return false;
+        }
+
+        // Sets buffer size, according to the number of lines to retrieve.
+        // This gives a performance boost when reading a few lines from the file.
+        if (!$adaptive) {
+            $buffer = 4096;
+        }
+        else {
+            $buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
+        }
+
+        // Jump to last character
+        fseek($f, -1, SEEK_END);
+
+        // Read it and adjust line number if necessary
+        // (Otherwise the result would be wrong if file doesn't end with a blank line).
+        if (fread($f, 1) !== "\n") {
+            --$lines;
+        }
+
+        // Start reading
+        $output = '';
+        $chunk = '';
+
+        // While we would like more.
+        while (ftell($f) > 0 && $lines >= 0) {
+
+            // Figure out how far back we should jump.
+            $seek = min(ftell($f), $buffer);
+
+            // Do the jump (backwards, relative to where we are).
+            fseek($f, -$seek, SEEK_CUR);
+
+            // Read a chunk and prepend it to our output.
+            $output = ($chunk = fread($f, $seek)) . $output;
+
+            // Jump back to where we started reading.
+            fseek($f, -mb_strlen($chunk, '8bit'), SEEK_CUR);
+
+            // Decrease our line counter.
+            $lines -= substr_count($chunk, "\n");
+
+        }
+
+        // While we have too many lines
+        // (Because of buffer size we might have read too many).
+        while ($lines++ < 0) {
+
+            // Find first newline and remove all text before that
+            $output = substr($output, strpos($output, "\n") + 1);
+
+        }
+
+        fclose($f);
+
+        return trim($output);
     }
 
     public function close(): bool
