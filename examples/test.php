2d9d681fc6c1b9748469bc5a0c5809e6436b947c
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2021-04-05 20:32:28 +0200

    Move examples to separate dir
    Less clutter

---
+++ examples/test.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+use steinmb\Alarm;
+use steinmb\BrewSession;
+use steinmb\BrewSessionConfig;
+use steinmb\EntityFactory;
+use steinmb\Logger\Curl;
+use steinmb\Logger\FileStorageHandler;
+use steinmb\Logger\JsonDecode;
+use steinmb\Logger\Logger;
+use steinmb\Logger\TelegramHandler;
+use steinmb\Onewire\OneWire;
+use steinmb\Onewire\Sensor;
+use steinmb\Onewire\Temperature;
+use steinmb\RuntimeEnvironment;
+use steinmb\SystemClock;
+
+include_once __DIR__ . '/vendor/autoload.php';
+
+RuntimeEnvironment::init();
+$batches = RuntimeEnvironment::getSetting('BATCH');
+$brewSessionConfig = new BrewSessionConfig($batches);
+$sensor = new Sensor(new OneWire(), new SystemClock(), new EntityFactory());
+$probes = (!$sensor->getTemperatureSensors()) ? exit('No probes found.'): $sensor->getTemperatureSensors();
+
+print_r($probes);
+foreach ($probes as $probe) {
+	$brewTemperature = new Temperature($sensor->createEntity($probe));
+	var_dump($brewTemperature );
+	echo $brewTemperature . PHP_EOL;
+}
+
+
