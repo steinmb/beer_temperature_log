3e77d371efd1ba8b6112e8f8cb256e5bba08073e
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-12-04 17:28:22 +0100

    Minor cleanup. Make example as small as possible.

--- examples/findSensors.php
+++ examples/findSensors.php
@@ -1,12 +1,10 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 /**
- * @file Example: lists all 1-Wire devices found.
+ * @file Example: Get all 1-Wire devices connected.
  */
-
-use steinmb\Onewire\OneWire;
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
-$oneWire = new OneWire();
-echo (string) $oneWire . PHP_EOL;
+$oneWire = new steinmb\Onewire\OneWire();
+echo $oneWire . PHP_EOL;
