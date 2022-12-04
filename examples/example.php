512fd4c537f9a759a338c0ed81cf95a49cef34c2
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-12-04 17:48:05 +0100

    Reformatting

--- examples/example.php
+++ examples/example.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 use steinmb\EntityFactory;
 use steinmb\Onewire\OneWire;
@@ -11,4 +13,4 @@ include_once __DIR__ . '/../vendor/autoload.php';
 RuntimeEnvironment::setSetting('BREW_ROOT', __DIR__);
 
 $sensor = new Sensor(new OneWire(), new SystemClock(), new EntityFactory());
-$probes = (!$sensor->getTemperatureSensors()) ? exit('No probes found.'): $sensor->getTemperatureSensors();
+$probes = (!$sensor->getTemperatureSensors()) ? exit('No probes found.') : $sensor->getTemperatureSensors();
