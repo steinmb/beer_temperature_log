eb992c1ce75b2b6f12eb4719bb03e39d5099b577
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-12-18 23:44:51 +0100

    PSR-12 reformatting and improved docblock

--- examples/findSensors.php
+++ examples/findSensors.php
@@ -1,10 +1,12 @@
-<?php declare(strict_types=1);
+<?php
 
 /**
- * @file Example: Get all 1-Wire devices connected.
+ *   Example on how to find all 1-Wire devices connected to the system.
  */
+
+declare(strict_types=1);
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
-$oneWire = new steinmb\Onewire\OneWire();
+$oneWire = new steinmb\Onewire\OneWire(__DIR__ . '/../tests/data_all_valid');
 echo $oneWire . PHP_EOL;
