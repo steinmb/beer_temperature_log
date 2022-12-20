4f8ea3bfc91928534d9b70f56214f9f3a3826080
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-12-20 20:16:27 +0100

    Make example file use demo data set.

--- examples/example.php
+++ examples/example.php
@@ -11,6 +11,7 @@ use steinmb\SystemClock;
 include_once __DIR__ . '/../vendor/autoload.php';
 
 RuntimeEnvironment::setSetting('BREW_ROOT', __DIR__);
+$oneWire = new OneWire(__DIR__ . '/../tests/data_all_valid');
 
-$sensor = new Sensor(new OneWire(), new SystemClock(), new EntityFactory());
+$sensor = new Sensor($oneWire, new SystemClock(), new EntityFactory());
 $probes = (!$sensor->getTemperatureSensors()) ? exit('No probes found.') : $sensor->getTemperatureSensors();
