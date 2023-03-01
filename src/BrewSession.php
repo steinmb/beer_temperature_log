d962497eb73eeab846bad18cb5f2b87095ac44b4
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2023-03-01 22:29:17 +0100

    Housekeeping

--- src/BrewSession.php
+++ src/BrewSession.php
@@ -1,27 +1,17 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace steinmb;
 
 final class BrewSession implements BrewSessionInterface
 {
-    public string $sessionId;
-    public string $probe;
-    public string $ambient;
-    public int|float $low_limit;
-    public int|float $high_limit;
-
     public function __construct(
-        string $sessionId = '',
-        string $probe = '',
-        string $ambient = '',
-        float $low_limit = 0,
-        float $high_limit = 0
-    )
-    {
-        $this->sessionId = $sessionId;
-        $this->probe = $probe;
-        $this->ambient = $ambient;
-        $this->low_limit = $low_limit;
-        $this->high_limit = $high_limit;
+        public string $sessionId = '',
+        public string $probe = '',
+        public string $ambient = '',
+        public int|float $low_limit = 0,
+        public int|float $high_limit = 0,
+    ) {
     }
 }
