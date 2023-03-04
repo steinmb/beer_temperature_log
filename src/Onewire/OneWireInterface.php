b7ac37b66ffc43862204397448ac5fd9b1740d1b
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2023-03-04 19:41:50 +0100

    Make PHPStand level 1 pass

--- src/Onewire/OneWireInterface.php
+++ src/Onewire/OneWireInterface.php
@@ -4,7 +4,6 @@ namespace steinmb\Onewire;
 
 interface OneWireInterface
 {
-    public function __construct(string $directory);
     public function allSensors(): array;
     public function temperatureSensors(): array;
     public function content(string $sensor): string;
