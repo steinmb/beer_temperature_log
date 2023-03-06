220006bfc2cf39140b813a011cf353cae7f40188
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2023-03-06 21:28:02 +0100

    Use sensor factory

--- examples/example.php
+++ examples/example.php
@@ -2,16 +2,17 @@
 
 declare(strict_types=1);
 
-use steinmb\EntityFactory;
 use steinmb\Onewire\OneWire;
-use steinmb\Onewire\Sensor;
+use steinmb\Onewire\SensorFactory;
 use steinmb\RuntimeEnvironment;
-use steinmb\SystemClock;
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
-RuntimeEnvironment::setSetting('BREW_ROOT', __DIR__);
+RuntimeEnvironment::init();
 $oneWire = new OneWire(__DIR__ . '/../tests/data_all_valid');
+$sensorFactory = new SensorFactory($oneWire);
+$sensors = $sensorFactory->allSensors();
 
-$sensor = new Sensor($oneWire, new SystemClock(), new EntityFactory());
-$probes = (!$sensor->getTemperatureSensors()) ? exit('No probes found.') : $sensor->getTemperatureSensors();
+foreach ($sensors as $sensor) {
+    echo $sensor->id . PHP_EOL;
+}
