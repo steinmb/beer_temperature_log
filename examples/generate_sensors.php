b822ccdbd7ea76664d03ad14ee8a2a7567a9eab6
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2023-03-12 22:09:01 +0100

    No reason to init settings. OneWire does not use/need it.

--- examples/generate_sensors.php
+++ examples/generate_sensors.php
@@ -2,15 +2,10 @@
 
 declare(strict_types=1);
 
-use steinmb\Onewire\OneWire;
-use steinmb\Onewire\SensorFactory;
-use steinmb\RuntimeEnvironment;
-
 include_once __DIR__ . '/../vendor/autoload.php';
 
-RuntimeEnvironment::init();
-$oneWire = new OneWire(__DIR__ . '/../tests/data_all_valid');
-$sensorFactory = new SensorFactory($oneWire);
+$oneWire = new steinmb\Onewire\OneWire(__DIR__ . '/../tests/data_all_valid');
+$sensorFactory = new steinmb\Onewire\SensorFactory($oneWire);
 $sensors = $sensorFactory->allSensors();
 
 foreach ($sensors as $sensor) {
